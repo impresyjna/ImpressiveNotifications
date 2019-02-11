07f780b3b866e62ba2f486f682e598155249106b
Asia Kasprzycka <impresyjna@gmail.com>
2019-02-11 22:27:39 +0100

    Initial Commit

---
+++ ImpressiveNotifications/ImpressiveNotifications.h
@@ -0,0 +1,19 @@
+//
+//  ImpressiveNotifications.h
+//  ImpressiveNotifications
+//
+//  Created by Asia Kasprzycka on 11/02/2019.
+//  Copyright © 2019 Asia Kasprzycka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImpressiveNotifications.
+FOUNDATION_EXPORT double ImpressiveNotificationsVersionNumber;
+
+//! Project version string for ImpressiveNotifications.
+FOUNDATION_EXPORT const unsigned char ImpressiveNotificationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImpressiveNotifications/PublicHeader.h>
+
+
