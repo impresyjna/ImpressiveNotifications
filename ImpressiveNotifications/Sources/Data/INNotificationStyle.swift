25dd95161314757503a972519f2020ee18fbdd67
Asia Kasprzycka <8388673+impresyjna@users.noreply.github.com>
2019-11-26 10:02:36 +0100

    Horizontal and vertical margin for whole notification

--- ImpressiveNotifications/Sources/Data/INNotificationStyle.swift
+++ ImpressiveNotifications/Sources/Data/INNotificationStyle.swift
@@ -14,16 +14,23 @@ public struct INNotificationStyle {
     let titleColor: UIColor?
     let descriptionColor: UIColor?
     let imageSize: CGSize?
+    let verticalMargin: CGFloat
+    let horizontalMargin: CGFloat
+    
     
     public init(cornerRadius: CGFloat? = nil,
          backgroundColor: UIColor? = nil,
          titleColor: UIColor? = nil,
          descriptionColor: UIColor? = nil,
-         imageSize: CGSize? = nil) {
+         imageSize: CGSize? = nil,
+         verticalMargin: CGFloat = 16.0,
+         horizontalMargin: CGFloat = 16.0) {
         self.cornerRadius = cornerRadius
         self.backgroundColor = backgroundColor
         self.titleColor = titleColor
         self.descriptionColor = descriptionColor
         self.imageSize = imageSize
+        self.verticalMargin = verticalMargin
+        self.horizontalMargin = horizontalMargin
     }
 }
